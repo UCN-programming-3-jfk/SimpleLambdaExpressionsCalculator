c10100fe367e2192a6960b1cf858893ecc954dae
Jakob Farian Krarup <kr@rup.dk>
2024-10-08 16:53:28 +0200

    Add project files.

---
+++ LambdaExpressions/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    public class Calculator
+    {
+        public float Add(float number1, float number2) => number1 + number2;
+        public float Subtract(float number1, float number2) => number1 - number2;
+        public float Multiply(float number1, float number2) => number1 * number2;
+        public float Divide(float number1, float number2)
+        {
+            if(number2 == 0)
+            {
+                throw new ArgumentException("Can't divide by zero!");
+            }
+            return number1 / number2;
+        }
+    }
+}
---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            float sum = calc.Add(3, 4);
+            float remainder = calc.Subtract(4, 3);
+            float product = calc.Multiply(6, 9);
+            float fraction = calc.Divide(42, 7);
+        }
+    }
+}
